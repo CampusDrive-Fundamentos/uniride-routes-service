9d049c9336f412cb0706695ac4576d5a747c787c
ErickLeo13 <u20231a500@upc.edu.pe>
2026-05-07 20:14:01 -0500

    Add files via upload

---
+++ src/main/java/com/uniride/unirideroutesservice/UnirideRoutesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uniride.unirideroutesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirideRoutesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnirideRoutesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniride/unirideroutesservice/UnirideRoutesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniride.unirideroutesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnirideRoutesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
